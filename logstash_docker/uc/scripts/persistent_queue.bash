1d2e5754401eb98031e94f566f4fd0b13a765127
Ofer-Haim <ofer.haim@ibm.com>
2020-08-02 14:07:02 +0300

    GRD-43432: Disabled persistent queue by default

---
+++ logstash_docker/uc/scripts/persistent_queue.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Use this script only when 1 pipeline with Persistent Queue is configured
+$pipelines = $UDS_ETC/pipelines.yml
+if [[ $1 = "on" ]]; then
+    echo "turning persistent queue on..."
+    sed -i -r "s/#queue.type: persisted/queue.type: persisted/g" $pipelines
+elif [[ $1 = "off" ]]; then
+    echo "turning persistent queue off..."
+    sed -i -r "s/queue.type: persisted/#queue.type: persisted/g" $pipelines
+else
+    echo "invalid parameters - first parameter can be only on / off"
+fi
+
