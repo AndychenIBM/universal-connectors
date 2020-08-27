83c7a097a70a4add1cf1d75455d6f35f39963743
Ofer-Haim <ofer.haim@ibm.com>
2020-08-27 12:18:55 +0300

    GRD-44741: Fixed install_customer_plugins.sh

--- logstash_docker/uc/scripts/install_customer_plugins.sh
+++ logstash_docker/uc/scripts/install_customer_plugins.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-FILE=${LOGSTASH_DIR}/customer/config/logstash-offline-plugins-7.5.0.zip
-if test -f "$FILE"; then
-    echo "preparing to install logstash-offline-plugins..."
-    logstash-plugin install file:///$FILE
-    echo "logstash-offline-plugins package is now installed and ready to use"
-else
-    echo "no logstash-offline-plugins zip file was found in plugins directory..."
-fi
+CUSTOMER_PLUGINS_DIR=${LOGSTASH_DIR}/customer/config/
+for file in "${CUSTOMER_PLUGINS_DIR}"/*.zip; do
+  [ -e "$file" ] || continue
+  echo "preparing to install ${file##*/}"
+  logstash-plugin install file:///$CUSTOMER_PLUGINS_DIR/${file##*/}
+  echo "done intalling ${file##*/}"
+done
