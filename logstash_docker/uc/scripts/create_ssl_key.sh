2a686fd8bf0769fb5b2822be68ee1cfd63fd95c9
Ofer-Haim <ofer.haim@ibm.com>
2020-10-07 20:51:26 +0300

    Added an env variable for SSL

--- logstash_docker/uc/scripts/create_ssl_key.sh
+++ logstash_docker/uc/scripts/create_ssl_key.sh
@@ -4,10 +4,10 @@ DEFAULT_UC_HOSTNAME="*.guard.swg.usma.ibm.com"
 UC_HOSTNAME=${1:-$DEFAULT_UC_HOSTNAME}
 
 #Create key,certificate in ${SSL_DIR}
-openssl req -x509 -batch -nodes -subj "/CN=${UC_HOSTNAME}/" -days 3650 -newkey rsa:2048 -keyout ${UC_EXTERNAL_CONFIG}/logstash-beats.key -out ${UC_EXTERNAL_CONFIG}/logstash-beats.crt
+openssl req -x509 -batch -nodes -subj "/CN=${UC_HOSTNAME}/" -days 3650 -newkey rsa:2048 -keyout ${SSL_DIR}/logstash-beats.key -out ${SSL_DIR}/logstash-beats.crt
 
 #Edit Filebeat ssl input plugin in Logstash configuration:
-SSL_CREDENTIALS_STR="ssl => true ssl_certificate => \"$UC_EXTERNAL_CONFIG/logstash-beats.crt\" ssl_key => \"$UC_EXTERNAL_CONFIG/logstash-beats.key\""
+SSL_CREDENTIALS_STR="ssl => true ssl_certificate => \"$SSL_DIR/logstash-beats.crt\" ssl_key => \"$SSL_DIR/logstash-beats.key\""
 
 #sed  -i "s/beats { port => 5044 type => filebeat }/beats { port => 5044 type => filebeat ${SSL_CREDENTIALS_STR//\//\\/} \}/g" ${LOGSTASH_DIR}/pipeline/mongodb-syslog-filebeat.conf
 
