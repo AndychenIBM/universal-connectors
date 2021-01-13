6f2902e666776d5f48c44971300bd1a9f290b2a1
Itai Gordon <ITAIG@il.ibm.com>
2021-01-13 18:41:23 +0200

    INS-8718: create travis draft

---
+++ buildDocketForTravis.sh
@@ -0,0 +1,3 @@
+cd logstash_docker
+docker pull sec-guardium-next-gen-docker-local.artifactory.swg-devops.com/universal-connector-base
+docker build -t universal-connector .  2>&1
---
+++ test/test.sh
@@ -0,0 +1,1 @@
+docker run -d --name="Klaus" --network="host" -e UC_LOG_LEVEL="DEBUG" -it universal-connector
