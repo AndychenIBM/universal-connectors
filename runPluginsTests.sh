ef861f0bfb1766777b2615922e6a209b2ff22862
Itai Gordon <ITAIG@il.ibm.com>
2021-01-17 18:49:43 +0200

    INS-8718-check test of output plugin

--- runPluginsTests.sh
+++ runPluginsTests.sh
@@ -5,5 +5,6 @@ chmod 755 ./gradlew
 ./gradlew jar
 cd ../logstash-output-guardium
 export UC_ETC=$(pwd)/src/resources
+cp ./gradle.propertiesTravis ./gradle.properties
 chmod 755 ./gradlew
 ./gradlew test
