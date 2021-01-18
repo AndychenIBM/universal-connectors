899772f1cb86daa56dcb533eaf3ad99d9b63446d
Itai Gordon <ITAIG@il.ibm.com>
2021-01-18 14:54:59 +0200

    INS-8718-check test of output plugin

--- runPluginsTests.sh
+++ runPluginsTests.sh
@@ -1,7 +1,17 @@
+echo AAA
+pwd
+cat /etc/os-release
+echo BBB
 cd logstash-uc-commons
 chmod 755 ./gradlew
 ./gradlew jar
 cd ../logstash-output-guardium
+rm -rf logstash
+mkdir logstash
+cd logstash
+sudo apt-get install ruby-full
+git clone -b 7.5 https://github.com/elastic/logstash.git
+cd ..
 export UC_ETC=$(pwd)/src/resources
 cp ./gradle.propertiesTravis ./gradle.properties
 chmod 755 ./gradlew
