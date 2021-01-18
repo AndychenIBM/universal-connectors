385b8a1433cdf15ca0f35653309027a6e881a375
Itai Gordon <ITAIG@il.ibm.com>
2021-01-18 19:50:53 +0200

    INS-8718-check test of output plugin

--- runPluginsTests.sh
+++ runPluginsTests.sh
@@ -11,10 +11,13 @@ mkdir logstash
 cd logstash
 sudo apt-get install ruby-full
 git clone -b 7.5 https://github.com/elastic/logstash.git
-cd ..
+cd logstash
+chmod 755 ./gradlew
+./gradlew :logstash-core:build
+cd ../../
 export UC_ETC=$(pwd)/src/resources
 cp ./gradle.propertiesTravis ./gradle.properties
-ls -la /home/travis/build/Activity-Insights/universal-connector/logstash-output-guardium/logstash/logstash/logstash-core
+ls -la /home/travis/build/Activity-Insights/universal-connector/logstash-output-guardium/logstash/logstash/logstash-core/build/libs
 echo CCCC
 find / -name "logstash-core*.jar"
 echo DDDDD
