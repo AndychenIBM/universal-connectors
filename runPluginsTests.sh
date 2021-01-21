590b2298c7e89d412629ffdf0283add1528e700c
Itai Gordon <ITAIG@il.ibm.com>
2021-01-21 16:25:59 +0200

    INS-8718-check test of output plugin

--- runPluginsTests.sh
+++ runPluginsTests.sh
@@ -1,33 +1,28 @@
-echo AAA
-pwd
-cat /etc/os-release
-echo BBB
+#build uc-commons
 cd logstash-uc-commons
 chmod 755 ./gradlew
 ./gradlew jar
+#build outputplugin
 cd ../logstash-output-guardium
 rm -rf logstash
 mkdir logstash
 cd logstash
+#install ruby and clone logstash
 sudo apt-get install ruby-full
 git clone -b 7.5 https://github.com/elastic/logstash.git
 cd logstash
 chmod 755 ./gradlew
 ./gradlew -q :logstash-core:build
+#use gradlefor travis
 cd ../../
-export UC_ETC=$(pwd)/src/resources
-ls -la $UC_ETC
-pwd
-echo HHH
-echo $UC_ETC
-ls -la
-echo JJJJ
 cp ./gradle.propertiesTravis ./gradle.properties
-ls -la /home/travis/build/Activity-Insights/universal-connector/logstash-output-guardium/logstash/logstash/logstash-core/build/libs
+#run output tests
 chmod 755 ./gradlew
 ./gradlew test --debug
+#run filter tests
 cp ./gradle.propertiesTravis ../logstash-filter-s3-guardium/gradle.properties
 cd ../logstash-filter-s3-guardium/
 chmod 755 ./gradlew
 ./gradlew test --debug
+#print resultreport to log
 cat /home/travis/build/Activity-Insights/universal-connector/logstash-output-guardium/build/reports/tests/test/index.html
