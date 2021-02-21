b0428e1b2b4e685fbf55a08d0ce18cbe3f8dde37
Ofer-Haim <ofer.haim@ibm.com>
2021-02-21 15:07:55 +0200

    GRD-49243: modified a script to clean customer pipeline

--- logstash_docker/uc/scripts/remove_customer_conf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf_backup
-rm -f ${LOGSTASH_DIR}/customer/pipeline/*.conf
-mv ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf_backup ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf
-
-
