b1f8e05def2a0e80ef4d9e5e71b73957ca54491e
Tal Daniel <taldan@il.ibm.com>
2021-07-01 19:18:17 +0300

    GRD-48510: adds install dependencies script (rpm)

---
+++ logstash_docker/uc/scripts/install_customer_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$GI_MODE" ]
+then
+    CUSTOMER_PLUGINS_DIR=${LOGSTASH_DIR}/customer/config/
+else
+    CUSTOMER_PLUGINS_DIR=${GI_PLUGINS_DIR}
+fi
+
+for file in "${CUSTOMER_PLUGINS_DIR}"/*.rpm; do
+  [ -e "$file" ] || continue
+  echo "preparing to install ${file##*/}"
+  rpm -ivh $CUSTOMER_PLUGINS_DIR/${file##*/}
+  status=$?
+  if [ $status -eq 0 ]; then
+    echo "done installing ${file##*/}"
+    exit 0
+  else
+    echo "An error occurred while trying to install a customer dependency."
+    exit $status
+  fi
+
+done
