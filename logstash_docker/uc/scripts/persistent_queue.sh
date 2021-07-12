d48915bab7d978ffab8a81d2806ec993f3933a1f
Ofer Haim <Ofer.Haim@ibm.com>
2021-07-12 08:57:27 +0300

    GRD-53298 (#52)
    * Revert "INS-11986: merged dockerfiles (#35)"
    
    This reverts commit 09b4f128
    
    * GRD-53298: reload logstash dur persistent queue modifications

--- logstash_docker/uc/scripts/persistent_queue.sh
+++ logstash_docker/uc/scripts/persistent_queue.sh
@@ -4,9 +4,12 @@ pipelines=$UC_ETC/pipelines.yml
 if [[ $1 = "on" ]]; then
     echo "turning persistent queue on..."
     sed -i -r "s/#queue.type: persisted/queue.type: persisted/g" $pipelines
+    sed -i -r "s/queue.type:.*/queue.type: persisted/g" $pipelines
+    "${UC_SCRIPTS}"/reload_logstash_conf.sh
 elif [[ $1 = "off" ]]; then
     echo "turning persistent queue off..."
     sed -i -r "s/queue.type: persisted/#queue.type: persisted/g" $pipelines
+    "${UC_SCRIPTS}"/reload_logstash_conf.sh
 else
     echo "invalid parameters - first parameter can be only on / off"
 fi
