d33d22aad593961f4c5f58ca7f8d9049a448a2b3
Ofer Haim <Ofer.Haim@ibm.com>
2021-07-26 14:20:38 +0300

    INS-13696: removed rpms in clean_customer_pipeline (#62)

--- logstash_docker/uc/scripts/clean_customer_pipeline.sh
+++ logstash_docker/uc/scripts/clean_customer_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mv ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf_backup
-rm -rf ${LOGSTASH_DIR}/customer/config/*.zip ${LOGSTASH_DIR}/customer/pipeline/*.conf
+rm -rf ${LOGSTASH_DIR}/customer/config/*.zip ${LOGSTASH_DIR}/customer/config/*.rpm ${LOGSTASH_DIR}/customer/pipeline/*.conf
 mv ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf_backup ${LOGSTASH_DIR}/customer/pipeline/output_to_guardium.conf
