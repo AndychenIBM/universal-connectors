019121874d54966b9d958434dfd8e2f4773ece7d
Ofer Haim <Ofer.Haim@ibm.com>
2022-02-20 19:26:12 +0200

    Grd 56395 (#170)
    * Revert "INS-11986: merged dockerfiles (#35)"
    
    This reverts commit 09b4f128
    
    * GRD-56395: allow multiple rpms installation
    
    * GRD-56395: added g=u permissions to /var/lib/rpm
    
    * GRD-56395: chown ${RPM_DIR} to guc:root
    
    * .

--- logstash_docker/uc/scripts/install_customer_dependencies.sh
+++ logstash_docker/uc/scripts/install_customer_dependencies.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+echo "GI_MODE" $GI_MODE
 if [ -z "$GI_MODE" ]
 then
-    echo "GI_MODE" $GI_MODE
     CUSTOMER_PLUGINS_DIR=${LOGSTASH_DIR}/customer/config/
     for file in "${CUSTOMER_PLUGINS_DIR}"/*.rpm; do
       [ -e "$file" ] || continue
@@ -11,15 +10,12 @@ then
       status=$?
       if [ $status -eq 0 ]; then
         echo "done installing ${file##*/}"
-        exit 0
       else
         echo "An error occurred while trying to install a customer dependency."
         exit $status
       fi
-
     done
 else
-    echo "GI_MODE" $GI_MODE
     CUSTOMER_PLUGINS_DIR=${GI_PLUGINS_DEPENDENCIES_DIR}
     for file in "${CUSTOMER_PLUGINS_DIR}"*/*.rpm; do
       [ -e $file ] || continue
@@ -28,12 +24,9 @@ else
       status=$?
       if [ $status -eq 0 ]; then
         echo "done installing ${file##*/}"
-        exit 0
       else
         echo "An error occurred while trying to install a customer dependency."
         exit $status
       fi
-
     done
-
 fi
