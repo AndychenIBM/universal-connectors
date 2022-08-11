3cf05edeaf6db2c084626925dfad4facaa9b3621
Dor Yosef <Dor.Yosef@ibm.com>
2022-08-11 17:28:04 +0300

    downloading uc-bootstrap from artifactory (#247)

--- buildUCDockerImageForTravis.sh
+++ buildUCDockerImageForTravis.sh
@@ -2,5 +2,6 @@ cp **/*.gem logstash_docker/uc/config
 docker  run  --name="Lio" -v `pwd`:`pwd` -w `pwd` -dit guc_dit:latest bash
 chmod -R 755 **/gradlew
 docker exec Lio bash -c "./prepareUCDefaultOfflinePackageInDocker.sh"
+./downloadUCBootstrapArtifact.sh
 cd logstash_docker
 docker build -t universal-connector .  2>&1
---
+++ downloadUCBootstrapArtifact.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# **************************************************************
+#
+# IBM Confidential
+#
+# OCO Source Materials
+#
+# 5737-L66
+#
+# (C) Copyright IBM Corp. 2022
+#
+# The source code for this program is not published or otherwise
+# divested of its trade secrets, irrespective of what has been
+# deposited with the U.S. Copyright Office.
+#
+# **************************************************************
+
+BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# constant
+VERSION=1.0.1
+BOOSTRAP_LOCATION=logstash_docker/uc/setup
+
+# main
+mkdir -p $BOOSTRAP_LOCATION
+echo "downloading uc-bootstrap JAR from artifactory with version: $VERSION"
+wget -q --user=$ARTIFACTORY_USERNAME --password=$ARTIFACTORY_PASSWORD https://na.artifactory.swg-devops.com/artifactory/sec-guardium-next-gen-team-gradle-local/com/ibm/guardium/ucbootstrap/$VERSION/ucbootstrap-$VERSION.jar -O $BOOSTRAP_LOCATION/uc-bootstrap.jar
+
+if [ $? -ne 0 ]; then
+  echo "failed to download uc-bootstrap artifact"
+  exit 1
+fi;
+echo "done download uc-bootstrap artifact"
