bc9b5c01ea3fadb8a162aaaf27c2e2cdccb4a28b
Ofer Haim <Ofer.Haim@ibm.com>
2022-08-21 15:59:52 +0300

    added mongo atlas to pluginsToBuild list (#253)
    * added mongo atlas to pluginsToBuild list
    
    * Update pluginsToBuild.txt
    
    * Update defaultOfflinePackagePlugins.txt
    
    * Update prepareUCDefaultOfflinePackageInDocker.sh
    
    * added mongo_atlas to default UC offline package
    
    * removed mongo atlas dir from internal repo
    
    * remove mongo offline-package dir

--- prepareUCDefaultOfflinePackageInDocker.sh
+++ prepareUCDefaultOfflinePackageInDocker.sh
@@ -8,6 +8,7 @@ cd /usr/share/logstash/config
 #../bin/logstash-plugin install logstash-output-java_output_to_guardium-1.3.2.gem
 ../bin/logstash-plugin install logstash-filter-mongodb_guardium_filter-0.6.6.gem
 ../bin/logstash-plugin install logstash-filter-mysql_filter_guardium-1.0.0.gem
-../bin/logstash-plugin prepare-offline-pack --output ./guardium_logstash-offline-plugins.zip --overwrite logstash-filter-logstash_filter_s3_guardium logstash-filter-mysql_filter_guardium logstash-filter-mongodb_guardium_filter logstash-input-cloudwatch_logs
+../bin/logstash-plugin install logstash-input-mongo_atlas_input-1.0.1.gem
+../bin/logstash-plugin prepare-offline-pack --output ./guardium_logstash-offline-plugins.zip --overwrite logstash-filter-logstash_filter_s3_guardium logstash-filter-mysql_filter_guardium logstash-filter-mongodb_guardium_filter logstash-input-cloudwatch_logs logstash-input-mongo_atlas_input
 ../bin/logstash-plugin list --verbose
 cp ./guardium_logstash-offline-plugins.zip $CURDIR/logstash_docker/uc/config/
