7b04ad81e7dc013485fd20b4a93701fc92326cf3
Ofer Haim <Ofer.Haim@ibm.com>
2022-08-22 10:33:09 +0300

    Prepare offline from external file (#254)
    * Revert "INS-11986: merged dockerfiles (#35)"
    
    This reverts commit 09b4f128
    
    * use defaultOfflinePackagePlugins.txt to determine offline pack content
    
    * cp list of plugins to build to /usr/share/logstash/config
    
    * .
    
    * .
    
    * ignoring output plugin
    
    * extracted foreach loop logic to a function
    
    * uncommented installation command

--- prepareUCDefaultOfflinePackageInDocker.sh
+++ prepareUCDefaultOfflinePackageInDocker.sh
@@ -1,14 +1,24 @@
-CURDIR=`pwd`
-cp logstash_docker/uc/config/*.gem /usr/share/logstash/config
+CURDIR=$(pwd)
+function installPlugin() {
+  line=$1
+  if [[ $1 =~ ^#.* ]]; then
+    echo "Ignoring the commented out following line: $1"
+  # TODO- remove output_to_guardium handling after turning it to an offline-package
+  elif [[ "$1" == *"output_to_guardium"* ]]; then
+    echo "Ignoring output_to_guardium plugin installation inside UC default offline package."
+  else
+    pluginName=${1##*/}
+    echo "installing ${pluginName}"
+    ../bin/logstash-plugin install "${pluginName}"
+    packages_list+=" ${pluginName%-*}"
+  fi
+}
+
+cp logstash_docker/uc/config/*.gem defaultOfflinePackagePlugins.txt /usr/share/logstash/config
 cd /usr/share/logstash/config
-#../bin/logstash-plugin install logstash-filter-mysql_percona_guardium_filter-1.0.6.gem
-../bin/logstash-plugin list --verbose
-../bin/logstash-plugin install logstash-input-cloudwatch_logs-1.0.3.gem
-../bin/logstash-plugin install logstash-filter-logstash_filter_s3_guardium-0.5.4.gem
-#../bin/logstash-plugin install logstash-output-java_output_to_guardium-1.3.2.gem
-../bin/logstash-plugin install logstash-filter-mongodb_guardium_filter-0.6.6.gem
-../bin/logstash-plugin install logstash-filter-mysql_filter_guardium-1.0.0.gem
-../bin/logstash-plugin install logstash-input-mongo_atlas_input-1.0.1.gem
-../bin/logstash-plugin prepare-offline-pack --output ./guardium_logstash-offline-plugins.zip --overwrite logstash-filter-logstash_filter_s3_guardium logstash-filter-mysql_filter_guardium logstash-filter-mongodb_guardium_filter logstash-input-cloudwatch_logs logstash-input-mongo_atlas_input
+while read line; do installPlugin $line; done <defaultOfflinePackagePlugins.txt
+echo "packages included in the offline package: $packages_list"
+
+../bin/logstash-plugin prepare-offline-pack --output ./guardium_logstash-offline-plugins.zip --overwrite $packages_list
 ../bin/logstash-plugin list --verbose
 cp ./guardium_logstash-offline-plugins.zip $CURDIR/logstash_docker/uc/config/
