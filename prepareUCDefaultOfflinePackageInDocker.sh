cb8d7c284ff9473900b78aae816b7ea21c8561e7
Ofer Haim <Ofer.Haim@ibm.com>
2022-09-01 16:15:48 +0300

    Shorten travis build time (#263)
    * Revert "INS-11986: merged dockerfiles (#35)"
    
    This reverts commit 09b4f128
    
    * test- install all plugins in logstash in one command
    
    * .
    
    * .
    
    * .
    
    * removed unnecessary rm command

--- prepareUCDefaultOfflinePackageInDocker.sh
+++ prepareUCDefaultOfflinePackageInDocker.sh
@@ -1,5 +1,5 @@
 CURDIR=$(pwd)
-function installPlugin() {
+function addPluginToInstallationList() {
   line=$1
   if [[ $1 =~ ^#.* ]]; then
     echo "Ignoring the commented out following line: $1"
@@ -8,20 +8,21 @@ function installPlugin() {
     echo "Ignoring output_to_guardium plugin installation inside UC default offline package."
   else
     pluginName=${1##*/}
-    echo "installing ${pluginName}"
-    ../bin/logstash-plugin install "${pluginName}"
     packages_list+=" ${pluginName%-*}"
   fi
 }
 
 cp logstash_docker/uc/config/*.gem defaultOfflinePackagePlugins.txt /usr/share/logstash/config
 cd /usr/share/logstash/config
-while read line; do installPlugin ${UC_OPENSOURCE_ROOT_DIR}/$line; done <defaultOfflinePackagePlugins.txt
+
+while read line; do addPluginToInstallationList ${UC_OPENSOURCE_ROOT_DIR}/$line; done <defaultOfflinePackagePlugins.txt
 
 #TODO- remove hardcoded installation of cloudwatch_logs after publishing it to open-source Github repo
-installPlugin logstash_docker/uc/config/logstash-input-cloudwatch_logs-1.0.3.gem
+addPluginToInstallationList logstash_docker/uc/config/logstash-input-cloudwatch_logs-1.0.3.gem
 
-echo "packages included in the offline package: $packages_list"
+echo "Installing packages on Logstash..."
+../bin/logstash-plugin install *.gem
+
+echo "Packages included in UC default offline package: $packages_list"
 ../bin/logstash-plugin prepare-offline-pack --output ./guardium_logstash-offline-plugins.zip --overwrite $packages_list
-../bin/logstash-plugin list --verbose
 cp ./guardium_logstash-offline-plugins.zip $CURDIR/logstash_docker/uc/config/
