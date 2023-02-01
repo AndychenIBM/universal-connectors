322abe72f9c04417ca7c41cd4f8128befd240268
Refael Adi <Refael.Adi@ibm.com>
2023-02-01 09:49:40 +0200

    Ins 26880 (#331)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * This script checks if the file "defaultOfflinePackagePlugins.txt" exists in the open source directory, and if so, it reads each line, extracts the plugin name and location, reads the version from a "VERSION" file, adds the version to the file name, and updates the line in the "defaultOfflinePackagePlugins.txt" file.
    
    * This script checks if the file "defaultOfflinePackagePlugins.txt" exists in the open source directory, and if so, it reads each line, extracts the plugin name and location, reads the version from a "VERSION" file, adds the version to the file name, and updates the line in the "defaultOfflinePackagePlugins.txt" file.
    
    * This script checks if the file "defaultOfflinePackagePlugins.txt" exists in the open source directory, and if so, it reads each line, extracts the plugin name and location, reads the version from a "VERSION" file, adds the version to the file name, and updates the line in the "defaultOfflinePackagePlugins.txt" file.
    
    * This script checks if the file "defaultOfflinePackagePlugins.txt" exists in the open source directory, and if so, it reads each line, extracts the plugin name and location, reads the version from a "VERSION" file, adds the version to the file name, and updates the line in the "defaultOfflinePackagePlugins.txt" file.
    
    * This script checks if the file "defaultOfflinePackagePlugins.txt" exists in the open source directory, and if so, it reads each line, extracts the plugin name and location, reads the version from a "VERSION" file, adds the version to the file name, and updates the line in the "defaultOfflinePackagePlugins.txt" file.
    
    * refer to the original main repo in public

--- buildUCpluginGemsInDocker.sh
+++ buildUCpluginGemsInDocker.sh
@@ -13,6 +13,27 @@ else
 fi
 }
 
+#Updates the "defaultOfflinePackagePlugins.txt" by adding the latest version to each plugin file name
+function verify_plugins_version() {
+# if "defaultOfflinePackagePlugins.txt" exists in the open source directory (means it taken from there)
+  if [ -e ${UC_PLUGINS_BUILD_FOLDER}/defaultOfflinePackagePlugins.txt ]
+  then
+  echo "Updating plugin file names with the latest version in the defaultOfflinePackagePlugins.txt file"
+  grep -v '^#' defaultOfflinePackagePlugins.txt | while read -r line; do
+      plugin_name=$(basename "${line}")
+      plugin_location=$(dirname "${line}")
+
+      # Read the current version from the VERSION file and store it in a variable
+      version_file_path="$UC_OPENSOURCE_ROOT_DIR/$plugin_location/VERSION"
+      version=$(cat "${version_file_path}")
+
+      new_line="$plugin_location/$plugin_name-$version.gem"
+      sed -i "s|$line|$new_line|g" defaultOfflinePackagePlugins.txt
+  done
+  fi
+}
+
+
 chmod -R 777 *
 cd test
 docker build -t guc_dit:latest .
@@ -27,6 +48,10 @@ chmod -R 777 ${UC_OPENSOURCE_ROOT_DIR}
 # copy lists of plugins to build and package in UC default offline-package
 force_copy_if_exists pluginsToBuild.txt
 force_copy_if_exists defaultOfflinePackagePlugins.txt
+verify_plugins_version
+
+echo "Final list in defaultOfflinePackagePlugins.txt:"
+cat defaultOfflinePackagePlugins.txt
 
 # Run UC build container to build plugins inside it
 docker run -e UC_OPENSOURCE_ROOT_DIR=${UC_OPENSOURCE_ROOT_DIR} --name="Alan" -v `pwd`:`pwd` -w `pwd` -dit guc_dit:latest bash
