241e7a670e6f90fea0d737e542793212ab13135a
Refael Adi <Refael.Adi@ibm.com>
2023-07-24 16:49:03 +0300

    Grd 71162 v2 (#401)
    * test
    
    * 1. fixed the Troubleshooting script
    2. added new cases to the error codes:
    
    * 1. fixed the Troubleshooting script
    2. added new cases to the error codes:

--- logstash_docker/uc/scripts/troubleshooting.sh
+++ logstash_docker/uc/scripts/troubleshooting.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
+TROUBLESHOOTING_IS_GENERATED_MESSAGE="Troubleshooting script is generated"
 DEFAULT_ERR_LOG=${LOG_GUC_DIR}logstash_stdout_stderr.log
 ERR_LOG=${1:-$DEFAULT_ERR_LOG}
 MAX_ERROR_LOGS_AMOUNT=1000
+LAST_OCCURRENCE_LINE=0
+
 
 function returnKnownErrorCodes(){
   pattern=$(echo "$1" | cut -d ";" -f 1)
   err_code=$(echo "$1" | cut -d ";" -f 2)
   err_msg=$(echo "$1" | cut -d ";" -f 3)
-
-  grep -q "$pattern" $ERR_LOG && echo "message: $err_msg" && exit $err_code;
+  tail -n +$LAST_OCCURRENCE_LINE $ERR_LOG | grep -q "$pattern" && echo "$err_msg" ;
 }
+
 
 # Check if Logstash is up
 logstash_pid=$("${UC_SCRIPTS}"/get_logstash_pid.sh)
 if [[ -z "$logstash_pid" ]]; then
   echo "logstash is down"
-else
-  echo "logstash is running"
 fi
 
+
 # Check if ERR_LOG exists
-if [[ -f "$ERR_LOG" ]]; then
-  echo "$ERR_LOG exists. Checking logs..."
-else
+if [[ ! -f "$ERR_LOG" ]]; then
   echo "$ERR_LOG doesn't exist."
   exit 0
 fi
+
 
 # Check length
 err_cnt=$(grep "ERROR" ${ERR_LOG} | wc -l)
@@ -34,6 +35,33 @@ if [ $err_cnt -gt $MAX_ERROR_LOGS_AMOUNT ]; then
    echo "More than ${MAX_ERROR_LOGS_AMOUNT} errors are found on ${ERR_LOG}"
 fi
 
+
+# Find the last occurrence of TROUBLESHOOTING_IS_GENERATED_MESSAGE in the error logs and keep the line number, set to 0 if not found
+LAST_OCCURRENCE_LINE=$(grep -n -q "$TROUBLESHOOTING_IS_GENERATED_MESSAGE" "$ERR_LOG" && grep -n "$TROUBLESHOOTING_IS_GENERATED_MESSAGE" "$ERR_LOG" | tail -n 1 | cut -d ':' -f 1 || echo 0)
+if [ $LAST_OCCURRENCE_LINE -ne 0 ]; then
+  last_occurrence_datetime=$(sed -n "${LAST_OCCURRENCE_LINE}p" "$ERR_LOG" | cut -d' ' -f1,2)
+  echo "Checking for errors since the last troubleshooting report was generated on $last_occurrence_datetime :"
+
+fi
+
+
 # Connection errors
-grep -v '^#' ${UC_SCRIPTS}/troubleshooting_error_codes.txt | while read -r line ; do returnKnownErrorCodes "$line"; done
+
+# Initialize a flag to keep track of whether any error patterns were found
+found_errors=false
+
+while IFS= read -r line; do
+  if returnKnownErrorCodes "$line"; then
+    found_errors=true
+  fi
+done < "${UC_SCRIPTS}/troubleshooting_error_codes.txt"
+
+# Check the flag after the loop and print the appropriate message
+if ! $found_errors; then
+  echo "No errors found in the logs."
+fi
+
+# Log messages with timestamps
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $TROUBLESHOOTING_IS_GENERATED_MESSAGE" >> "$DEFAULT_ERR_LOG"
+
 exit 0
